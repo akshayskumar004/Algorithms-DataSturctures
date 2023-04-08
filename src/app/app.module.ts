e2f55fa8b5fb8e9440c36b2069581901f2cbf229
akshayskumar004 <akshayskumar004@gmail.com>
2023-04-08 23:00:46 +0530

    Javascript algorithms sorting methods

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SortingAlgorithmsComponent } from './sorting-algorithms/sorting-algorithms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SortingAlgorithmsComponent
   ],
   imports: [
     BrowserModule
